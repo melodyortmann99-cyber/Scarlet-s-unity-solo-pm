98bfb5107816fee6aeec60b9301246b835d10b91
melodyortmann99-cyber <melodyortmann99@gmail.com>
2025-09-11 13:43:42 -0700

    idk

--- Celestial Folder/Assets/Scripts/PlayerController.cs
+++ Celestial Folder/Assets/Scripts/PlayerController.cs
@@ -3,21 +3,46 @@ using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    Vector3 cameraOffset = new Vector3(0, .5F, .2f);
+    Vector2 cameraRotation = Vetor2.zero;
+    Camera playerCam;
+    InputAction lookAxis;
     Rigidbody rb;
 
     float inputX;
     float inputY;
 
+    public float Xsensitivity = .1f;
+    public float Ysensitivity = .1f;
     public float speed = 5f;
+    public float camRotationLimit = 90;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerCam = Camera.main;
+        lookAxis = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Camera Handler
+        playerCam.transform.position = transform.position + cameraOffset;
+
+        cameraRotation.x += lookAxis.ReadValue<Vector2>().x * Xsensitivity;
+        cameraRotation.y += lookAxis.ReadValue<Vector2>().y * Ysensitivity;
+
+        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -camRotationLimit, camRotationLimit);4
+
+        playerCam.transform.rotation = Quaternion.Euler(-camRotationLimit.y, cameraRotation.x, 0);
+        transform.rotation = Quaternion.AngleAxis(camaRotation.x, Vector3.up);
+
+        // Movement System
         Vector3 tempMove = rb.linearVelocity;
 
         tempMove.x = inputY * speed;
