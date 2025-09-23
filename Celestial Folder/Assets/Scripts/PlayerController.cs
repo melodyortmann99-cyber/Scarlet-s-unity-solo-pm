4331f26d99d7379c6b218d500d9534301c54e32a
melodyortmann99-cyber <melodyortmann99@gmail.com>
2025-09-23 13:38:23 -0700

    noted come to school with coffee everyday

--- Celestial Folder/Assets/Scripts/PlayerController.cs
+++ Celestial Folder/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,13 @@ public class PlayerController : MonoBehaviour
     InputAction lookAxis;
     public Rigidbody rb;
     Ray jumpRay;
+    Ray interactRay;
+    RaycastHit interactHit;
+    GameObject pickupObj;
+
+    public PlayerInput input;
+    public Transform weaponSlot;
+    public Weapon currentWeapon;
 
     float inputX;
     float inputY;
@@ -17,15 +24,22 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public float jumpHeight = 10F;
     public float jumpRayDistance = 1.1f;
+    public float interactDistance = 1f;
 
     public int Health = 5;
     public int maxHealth = 5;
+
+    public bool attacking = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        input = GetComponent<PlayerInput>();
+        interactRay = new Ray(transform.position, transform.forward);
+        jumpRay = new Ray(transform.position, -transform.up);
         rb = GetComponent<Rigidbody>();
         playerCam = Camera.main;
         lookAxis = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
+        weaponSlot = playerCam.transform.GetChild(0);
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -47,6 +61,23 @@ public class PlayerController : MonoBehaviour
         jumpRay.origin = transform.position;
         jumpRay.direction = -transform.up;
 
+        interactRay.origin = playerCam.transform.position;
+        interactRay.direction = playerCam.transform.forward;
+
+        if (Physics.Raycast(interactRay, out interactHit, interactDistance))
+        {
+            if (interactHit.collider.tag == "weapon")
+            {
+                pickupObj = interactHit.collider.gameObject;
+            }
+        }
+        else
+            pickupObj = null;
+
+        if (currentWeapon)
+            if (currentWeapon.holdToAttack && attacking)
+                currentWeapon.fire();
+
         // Movement System
         Vector3 tempMove = rb.linearVelocity;
 
@@ -56,7 +87,43 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = (tempMove.x * transform.forward) + (tempMove.y * transform.up) + (tempMove.z * transform.right);
 
     }
-
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if (currentWeapon)
+        {
+            if (currentWeapon.holdToAttack)
+            {
+                if (context.ReadValueAsButton())
+                    attacking = true;
+                else
+                    attacking = false;
+            }
+            else if (context.ReadValueAsButton())
+                currentWeapon.fire();
+        }
+    }
+    public void Reload()
+    {
+        if (currentWeapon)
+            currentWeapon.reload();
+    }
+    public void Interact()
+    {
+        if (pickupObj)
+        {
+            if (pickupObj.tag == "weapon")
+                pickupObj.GetComponent<Weapon>().equip(this);
+        }
+        else
+            Reload();
+    }
+    public void DropWeapon()
+    {
+        if (currentWeapon)
+        {
+            currentWeapon.GetComponent<Weapon>().unequip();
+        }
+    }
     public void Move(InputAction.CallbackContext context)
     {
         Vector2 InputAxis = context.ReadValue<Vector2>();
